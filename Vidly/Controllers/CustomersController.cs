78e905f5347665aa772c9d9de7973fd054120465
Amine Zeghad <zeghad.amine@yahoo.fr>
2019-04-23 00:39:34 +0200

    ok

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -74,6 +74,7 @@ namespace Vidly.Controllers
             var membershipTypes = _context.MemberShipTypes.ToList();
             var viewModel = new CustomerFormViewModel
             {
+                Customer = new Customer(),
                 MembershipTypes = membershipTypes
             };
 
